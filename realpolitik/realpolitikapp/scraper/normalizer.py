ee9f63a84c63759a3aec621d33cb2188ca1acfe0
Natasha Whitney <natiwhitney@gmail.com>
2019-02-26 18:46:05 -0500

    clean up normalizer a bit

--- realpolitik/realpolitikapp/scraper/normalizer.py
+++ realpolitik/realpolitikapp/scraper/normalizer.py
@@ -10,8 +10,9 @@ def _main(name):
 # TO DO
 # - duration
 
-# TK, iterate only thru existing args
-# otherwise skip? how to handle skip well
+
+# TKTK better error handling for skip
+# like a checksum
 def parse_args_wrapper(x, m):
   for args in m['args-array']:
     try:
@@ -32,6 +33,13 @@ def normalize_content(raw_csv_dict, name, content_type):
     normalized_content.update({c: vs})
   return transpose_dict_of_lists(normalized_content)
 
+def venue_join(event, venue):
+  event_venue_name = event['venue-name']
+  venue_venue_name = venue['venue-name']
+  ## TKTK use tokenizer instead of substring compare
+  ((event_venue_name in venue_venue_name) |
+   (venue_venue_name in event_venue_name)) &
+  event['city'] is venue['city']
 # TK add duration
 # TK a join is going to be a helluvalot faster
 def normalize_source(name, raw_events_dict, raw_venues_dict):
@@ -39,12 +47,10 @@ def normalize_source(name, raw_events_dict, raw_venues_dict):
   normalized_venues = normalize_content(raw_venues_dict, name, 'venues')
   for event in normalized_events:
     event.update({'duration': "WIP"}) # duration deal with later
-    event_venue_name = event['venue-name']
     venue_address = "no match"
     for venue in normalized_venues:
       venue_venue_name = venue['venue-name']
-      ## TKTK use tokenizer instead of substring compare
-      if ((event_venue_name in venue_venue_name) | (venue_venue_name in event_venue_name) & (event['city'] is venue['city'])):
+      if venue_join(event, venue):
         venue_address = venue['venue-address']
         break
     event.update({'venue-address': venue_address})
