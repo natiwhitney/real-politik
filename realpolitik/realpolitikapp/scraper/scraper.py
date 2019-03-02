34acfe0f7cd21a9e2683c3f2ba9a93a20e63cb2a
Natasha Whitney <natiwhitney@gmail.com>
2019-03-02 16:21:18 -0500

    Scraper import yml; standalone with _main

--- realpolitik/realpolitikapp/scraper/scraper.py
+++ realpolitik/realpolitikapp/scraper/scraper.py
@@ -1,37 +1,31 @@
-from bs4 import BeautifulSoup
+import sys
 import requests
-from metadata import sources_metadata
-
-def _main(name):
-  source_metadata = sources_metadata[name]
-  page_content = scrape_page(source_metadata['url'],source_metadata['parser'])
-
-  event_metadata = source_metadata['page-format']['events']
-  events = process_content(page_content, event_metadata, name)
-
-  venue_metadata = source_metadata['page-format']['venues']
-  venues = process_content(page_content,venue_metadata, name)
-
-  return {'events': events, 'venues': venues}
+from bs4 import BeautifulSoup
+from configs.load_metadata import sources_metadata
+from csvloader import write_data
 
 def scrape_page(url, parser):
   page_response = requests.get(url, timeout=5)
   page_content = BeautifulSoup(page_response.content, parser)
   return page_content
 
+
 def find_table(page_content):
   return page_content.find_all("table")
+
 
 def find_content(page_content, content_metadata):
   # Given BeautifulSoup page object, find relevant content
   # Find function is selected based on content's format key
   switcher = {
-    "table": find_table,
-    # different sources will require different formats (won't all be HTML tables)
+      "table": find_table,
+      # diff srcs-> diff formats
   }
-  content_format = content_metadata['format']
-  func = switcher.get(content_format, lambda: "Invalid format:" + content_format)
+  content_format = content_metadata['html-element']
+  func = switcher.get(content_format,
+                      lambda: "Invalid format:" + content_format)
   return func(page_content)
+
 
 def convert_html_table_to_dict(table):
   headers = []
@@ -50,25 +44,27 @@ def convert_html_table_to_dict(table):
         d_curr.append(e.text)
         d0 = {h: d_curr}
         d.update(d0)
-        i = i+1
+        i = i + 1
   return d
+
 
 def convert_content(content, content_metadata):
   # Given BeautifulSoup HTML tree(s), convert to dict(s)
   # Conversion function is selected based on content's format key
-  content_format = content_metadata['format']
+  content_format = content_metadata['html-element']
   content_indices = content_metadata['indices']
   switcher = {
-    "table": convert_html_table_to_dict,
-    # different sources will require different formats (won't all be HTML tables)
+      "table": convert_html_table_to_dict,  # diff srcs -> diff formats
   }
   html_dicts = []
-  for ind in content_indices: # there may be multiple tables, i.e. 19hz
+  for ind in content_indices:   # there may be multiple tables, i.e. 19hz
     content_html = content[ind]
-    func = switcher.get(content_format, lambda: "Invalid format:" + specific_format)
+    func = switcher.get(content_format,
+                        lambda: "Invalid format:" + content_format)
     content_dict = func(content_html)
     html_dicts.append(content_dict)
   return html_dicts
+
 
 def convert_to_raw_dict(content_dict, url_mappings, name):
   # Given unprocessed HTML dict, use source-specific mappings
@@ -91,16 +87,42 @@ def convert_to_raw_dict(content_dict, url_mappings, name):
           e = e + content_value
       elems.append(e)
     raw_dict.update({k: elems})
-    raw_dict.update({'source': [name for i in range(0,n_elems)]})
+    raw_dict.update({'source': [name for i in range(0, n_elems)]})
   return raw_dict
+
 
 # TKTK known bug... currently including headers twice
 def process_content(page_content, content_metadata, name):
   content = find_content(page_content, content_metadata)
   html_dicts = convert_content(content, content_metadata)
-  content_mappings = content_metadata['mappings']
+  content_mappings = content_metadata['url-mappings']
   raw_dicts = []
   for html_dict in html_dicts:
     raw_dict = convert_to_raw_dict(html_dict, content_mappings, name)
     raw_dicts.append(raw_dict)
   return raw_dicts
+
+def scrape(name):
+  source_metadata = sources_metadata[name]
+  print(source_metadata)
+  page_content = scrape_page(source_metadata['url'], source_metadata['parser'])
+
+  event_metadata = source_metadata['page-format']['events']
+  events = process_content(page_content, event_metadata, name)
+
+  venue_metadata = source_metadata['page-format']['venues']
+  venues = process_content(page_content, venue_metadata, name)
+
+  return {'events': events, 'venues': venues}
+
+
+def main():
+  name = sys.argv[1]
+  raw_dicts = scrape(name)
+  for events in raw_dicts['events']:
+    write_data('raw_events.csv', events)
+  for venues in raw_dicts['venues']:
+    write_data('raw_venues.csv', venues)
+
+if __name__ == '__main__':
+    main()
